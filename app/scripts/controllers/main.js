4c8c484ff0ebb42729b0ead9c888a05ab2b662c2
Aaron Smith <thevandytestbank@gmail.com>
2013-12-10 23:51:12 -0600

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('vandyTestBankApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
